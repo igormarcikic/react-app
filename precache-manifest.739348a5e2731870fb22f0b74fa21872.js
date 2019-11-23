c46bb7bb94a6611dee8fbffc9db54d5ce151f030
Igor <marcikicigor@gmail.com>
2019-11-23 14:44:43 +0100

    Updates

---
+++ precache-manifest.739348a5e2731870fb22f0b74fa21872.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b1eb7dbf70880362b5459a381d5837fb",
+    "url": "link-to-your-repository/index.html"
+  },
+  {
+    "revision": "0ae92c99cdb52325c4e1",
+    "url": "link-to-your-repository/static/css/2.b0bb7f2c.chunk.css"
+  },
+  {
+    "revision": "ca681fbcfd0a1d9f3d4f",
+    "url": "link-to-your-repository/static/css/main.7aa3e8df.chunk.css"
+  },
+  {
+    "revision": "0ae92c99cdb52325c4e1",
+    "url": "link-to-your-repository/static/js/2.985dd121.chunk.js"
+  },
+  {
+    "revision": "ca681fbcfd0a1d9f3d4f",
+    "url": "link-to-your-repository/static/js/main.7231f2ba.chunk.js"
+  },
+  {
+    "revision": "86ee73b80feab5b1cd0e",
+    "url": "link-to-your-repository/static/js/runtime-main.2787bf8e.js"
+  }
+]);
